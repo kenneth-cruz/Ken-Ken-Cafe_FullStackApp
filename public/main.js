10ee422e131169bfb12ad122e39c7c961cc0e5cb
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-07 22:21:43 -0400

    edits

--- public/main.js
+++ public/main.js
@@ -1,54 +1,35 @@
-// Credits: Aiperi Eldar, Sam Lawson, and Ryan "RyRy"
-// for helping our team figure out the text to speech software as well as the proper
-// logic for the PUTS / GETS in the CRUD functionality.
 
 
 var thumbUp = document.getElementsByClassName("fas fa-mug-hot");
 var thumbDown = document.getElementsByClassName("fas fa-volume-up");
 var trash = document.getElementsByClassName("fa-trash");
 
-
-
-//Related to put/messages
-Array.from(thumbUp).forEach(function(element) { //for each item called thumbs up (icon)
+Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
-        // name inside of a span, inside of li, indise of ul, from the UL back down to chose an li
-       //  const name =
-       // this.parentNode.parentNode.childNodes[1].innerText //name isinside span and span inside of an li and li inside unordered list so going from where it is up to the li and up to the ul and then from the ul back down it can choose any of the li's.
-       // //Indexes alternate because:*********** request[0], answer[1], req[2], answer[3] and so on
-       //
-       // //think of styling css- same concept, cascading down thetagsto get to the actual target tag
-       //  const order = this.parentNode.parentNode.childNodes[3].innerText
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const order = this.parentNode.parentNode.childNodes[3].innerText
         const status = this.parentNode.parentNode.childNodes[17].innerText
-        // const barista = this.parentNode.parentNode.childNodes[19].innerText
         console.log(status)
-        // const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText) //parsefloat= string to integer rather than concatinatingit will add properly.
         fetch('orders', {
           method: 'put',
-          headers: {'Content-Type': 'application/json'}, //is the headers section by convention?
+          headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
             'name': name,
             'order': order,
             'status':status,
-
-            // 'barista': barista
           })
         })
 
         .then(response => {
-          if (response.ok) return response.json() //take the responses if they are ok, pass them into the dom
+          if (response.ok) return response.json()
         })
         .then(data => {
           console.log(data)
-          window.location.reload(true)//refresh
+          window.location.reload(true)
         })
       });
 
 });
-// mirror image of thumbUp ...the difference is what is done to the
-//number in the routes.js
 Array.from(thumbDown).forEach(function(element) {
       element.addEventListener('click', function(){
         // const name = this.parentNode.parentNode.childNodes[1].innerText
--- server.js
+++ server.js
@@ -1,46 +1,29 @@
 
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080; //localhost url, able to change if need be
+var port     = process.env.PORT || 8080;
 const MongoClient = require('mongodb').MongoClient
-//server node modules. In order to run
-var mongoose = require('mongoose'); //Mongoose is an Object Data Modeling (ODM) library for MongoDB and Node.
-var passport = require('passport');//Passport is authentication middleware for Node. js.
-var flash    = require('connect-flash');//FlashJS is JavaScript graphics and game development engine with API similar to Flash one
-
-var morgan       = require('morgan');//Morgan: is another HTTP request logger middleware for Node. js.
-var cookieParser = require('cookie-parser');//cookie-parser is a middleware which parses cookies attached to the client request object.
-// bodyParser allows us to select only the information that we want to grab
-
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-// In order to get access to the post data we have to use body-parser. Basically what the body-parser is which allows express to read the body and then parse that into a Json object that we can understand.
 var session      = require('express-session');
-
-var configDB = require('./config/database.js'); // this is the file you need to go to connect to your own personal server
+var configDB = require('./config/database.js');
 
 var db
 
 // configuration ===============================================================
-mongoose.connect(configDB.url, (err, database) => { // .connect is a method used for mongoDB to connect to your database
+mongoose.connect(configDB.url, (err, database) => {
   if (err) return console.log(err)
   db = database
   require('./app/routes.js')(app, passport, db);
-}); // connect to our database
+});
 
-//app.listen(port, () => {
-    // MongoClient.connect(configDB.url, { useNewUrlParser: true }, (error, client) => {
-    //     if(error) {
-    //         throw error;
-    //     }
-    //     db = client.db(configDB.dbName);
-    //     console.log("Connected to `" + configDB.dbName + "`!");
-    //     require('./app/routes.js')(app, passport, db);
-    // });
-//});
 
-require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport);
 
-// set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
