82f52a8905b858ad0c14a4a71e5f8b43c36dc3e9
Kenneth Cruz <kenneth.s.cruz@gmail.com>
2020-05-07 22:18:49 -0400

    edits

--- config/database.js
+++ config/database.js
@@ -1,8 +1,4 @@
-// config/database.js
 module.exports = {
-
-// we inserted our Barista database.
-
-    'url' : 'mongodb+srv://demo:demo@barista-euexy.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb+srv://demo:demo@barista-euexy.mongodb.net/test?retryWrites=true&w=majority',
     'dbName': 'orders'
 };
